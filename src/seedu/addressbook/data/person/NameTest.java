a91813bac9477de57e0c482120eebf1aef6e2f12
azoic0 <yingyu0113@gmail.com>
2016-09-08 21:52:24 +0800

    test isSimilar

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -1,6 +1,9 @@
 package seedu.addressbook.data.person;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
@@ -11,6 +14,11 @@ public class NameTest {
 	@Before
 	public void setup() throws IllegalValueException{
 		name = new Name("John K Smith");
+	}
+	
+	@Test
+	public void isSimilar_true(){
+		assertTrue(name.isSimilar(null));
 	}
 
 }
