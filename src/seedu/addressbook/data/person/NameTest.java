e44dd38f975bd9a4252a5199f06ffa21279df1fe
azoic0 <yingyu0113@gmail.com>
2016-09-08 21:47:49 +0800

    setup Junit

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -1,5 +1,16 @@
 package seedu.addressbook.data.person;
 
+import org.junit.Before;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class NameTest {
+	
+	private Name name;
+	
+	@Before
+	public void setup() throws IllegalValueException{
+		name = new Name("John K Smith");
+	}
 
 }
